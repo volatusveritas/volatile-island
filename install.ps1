b7af26e29fa11d198d08cc1efaf12f39380751a3
lucasmartinscopas <lucas.martins@copastur.com.br>
2022-03-09 14:42:35 -0300

    First version of the install script

---
+++ install.ps1
@@ -0,0 +1,5 @@
+# Neovim's Vimrc
+Copy-Item dotfiles\neovim\init.vim ~\AppData\Local\nvim\init.vim
+
+# PowerShell profile
+Copy-Item dotfiles\powershell\profile.ps1 ~\Documents\PowerShell\profile.ps1
