ae03e3472969ab2f86deeba7bf980af525d6288e
Volatus <volatusCoder@outlook.com>
2022-08-08 01:15:16 -0300

    Install multiple files (neovim)

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
-# Neovim's Vimrc
-Copy-Item dotfiles\neovim\init.vim ~\AppData\Local\nvim\init.vim
+# Neovim's Init
+Copy-Item dotfiles\neovim\* ~\AppData\Local\nvim\
 
 # PowerShell profile
 Copy-Item dotfiles\powershell\profile.ps1 ~\Documents\PowerShell\profile.ps1
