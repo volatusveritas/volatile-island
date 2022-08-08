af894541e42d849c0ad9b49c95ee6c2d741727e4
Volatus <volatusCoder@outlook.com>
2022-08-08 01:18:03 -0300

    Make copy calls recursive

--- collect.ps1
+++ collect.ps1
@@ -1,5 +1,5 @@
 # Neovim's Init
-Copy-Item ~\AppData\Local\nvim\* dotfiles\neovim\
+Copy-Item -Recurse ~\AppData\Local\nvim\* dotfiles\neovim\
 
 # PowerShell profile
 Copy-Item ~\Documents\PowerShell\profile.ps1 dotfiles\powershell\profile.ps1
--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 # Neovim's Init
-Copy-Item dotfiles\neovim\* ~\AppData\Local\nvim\
+Copy-Item -Recurse dotfiles\neovim\* ~\AppData\Local\nvim\
 
 # PowerShell profile
 Copy-Item dotfiles\powershell\profile.ps1 ~\Documents\PowerShell\profile.ps1
